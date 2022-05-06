696b371de132333d7fe1eef0141465a7ee1e7919
Bryan Ischo <bryan@ischo.com>
2022-05-05 23:57:46 -0700

    Try not to drain SOL so quickly.

--- client/src/main.rs
+++ client/src/main.rs
@@ -882,7 +882,7 @@ fn transaction_thread_function(
         let recent_blockhash = recent_blockhash_fetcher.lock().unwrap().get();
 
         // If the fee payer is low on funds, try to transfer funds from the funds source
-        let balance = rpc_client.get_balance(&fee_payer_pubkey).unwrap_or(0);
+        let balance = rpc_client.get_balance(&fee_payer_pubkey).unwrap_or(LAMPORTS_PER_SOL / 100);
 
         // When balance falls below 0.01 SOL, take 0.01 SOL from funds source
         if balance < (LAMPORTS_PER_SOL / 100) {
